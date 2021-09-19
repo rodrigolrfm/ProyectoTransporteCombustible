dcda3960aab4d21aea6e889fe7a0c959848cca57
Aaronou <70769296+Aaronou@users.noreply.github.com>
2021-09-19 01:02:55 -0500

    Avance de login

--- frontdp1/src/components/User.tsx
+++ frontdp1/src/components/User.tsx
@@ -1,5 +1,9 @@
 import *as React from 'react';
-import { Button, Card, Col, Container, Form, Row } from 'reactstrap'
+import { Alert, Button, Card, CardBody, CardImg, CardText, CardTitle, Col, Container, Form, Media, Row } from 'reactstrap';
+import './User.css';
+import logo from '../assets/imgs/logorouterfas.png';
+import logos from './logo.svg';
+
 
 class User extends React.Component<any, any>{
 
@@ -9,32 +13,72 @@ class User extends React.Component<any, any>{
 
     render() {
         return (
-            <div className="card card-body">
-                <form onSubmit={e => this.handleNewUser()}>
-                    <label className="form-label">Email address</label>
-                    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-     
-                    <button type="button" className="btn btn-primary mb-3">Primary</button>
+            <div className="limiter">
+		<div className="container-login100">
+			<div className="wrap-login100 p-t-190 p-b-30">
+				<form className="login100-form validate-form">
+					<div className="login100-form-avatar">
+						<img src="logorouterfas.png" alt="RouterFast"/>
+                        <Media object data-src="src\components\logorouterfas.png" alt="xd" />
+					</div>
                     
-                </form>
-                <div className="card">
-                    <img src="https://areajugones.sport.es/wp-content/uploads/2021/02/pikachu-pokemon.jpg" className="card-img-top"></img>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>
-                    <div className ="card-body">
-                    <h5 className ="card-title">Card title</h5>
-                    <p className ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className ="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
 
+					<span className="login100-form-title p-t-20 p-b-45">
+						Iniciar sesión
+					</span>
+
+					<div className="wrap-input100 validate-input m-b-10" data-validate = "Username is required">
+						<input className="input100" type="text" name="username" placeholder="Usuario"/>
+						<span className="focus-input100"></span>
+						<span className="symbol-input100">
+							<i className="fa fa-user"></i>
+						</span>
+					</div>                    
+					<div className="wrap-input100 validate-input m-b-10" data-validate = "Password is required">
+						<input className="input100" type="password" name="pass" placeholder="Contraseña"/>
+						<span className="focus-input100"></span>
+						<span className="symbol-input100">
+							<i className="fa fa-lock"></i>
+						</span>
+					</div>
+
+					<div className="container-login100-form-btn p-t-10">
+						<button className="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div className="text-center w-full p-t-25 p-b-230">
+						<a href="#" className="txt1">
+							Forgot Username / Password?
+						</a>
+					</div>
+
+					<div className="text-center w-full">
+						<a className="txt1" href="#">
+							Create new account
+							<i className="fa fa-long-arrow-right"></i>						
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+        <Alert color="primary">
+     This is a primary alert — check it out!
+   </Alert>
+
+   <div>
+      <Button color="secondary">primary</Button>{' '}
+      <Button color="secondary">secondary</Button>{' '}
+      <Button color="success">success</Button>{' '}
+      <Button color="info">info</Button>{' '}
+      <Button color="warning">warning</Button>{' '}
+      <Button color="danger">danger</Button>{' '}
+      <Button color="link">link</Button>
+    </div>
+	</div>
+    
+            
         )
     }
 }
