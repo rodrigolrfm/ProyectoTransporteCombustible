a6c23770f5328b2298921fb8399a0cabda646689
Aaronou <70769296+Aaronou@users.noreply.github.com>
2021-09-21 23:30:03 -0500

    Limpieza e Instalacion de iconos-ui

--- Frontend/router-fast-front/src/App.tsx
+++ Frontend/router-fast-front/src/App.tsx
@@ -1,28 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from '@mui/material/Button';
-
+import { Button } from "@mui/material";
+import React from "react";
+import ButtonUnstyled from "@mui/core/ButtonUnstyled";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Button variant="text">Text</Button>
-<Button variant="contained">Contained</Button>
-<Button variant="outlined">Outlined</Button>
+      <Button variant="contained" color="secondary">
+        Contained
+      </Button>
+      <Button variant="outlined" color="secondary" size="large">
+        Outlined
+      </Button>
     </div>
   );
 }
