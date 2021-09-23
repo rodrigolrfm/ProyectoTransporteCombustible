3aab6a62d45391b8e95867c564364b047debbab0
rodrigolrfm <31398550+rodrigolrfm@users.noreply.github.com>
2021-09-22 22:48:01 -0500

    Rodrigo

---
+++ Algortimos/AlgoritmoCVRP/src/main/java/DistanceMatrix.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DistanceMatrix {
+	public int[][] map;
+	public int mapSizeX;
+	public int mapSizeY;
+	public int[] depots;
+	public int[] clients;
+	public long[][] matrix;
+	public int matrixSize;
+	public final int K = 1;
+	public List<Node> bloqueados;
+	public List<Node> nodos;
+
+	public DistanceMatrix(int[][] map, int mapSizeX, int mapSizeY) {
+		super();
+		this.map = map;
+		this.mapSizeX = mapSizeX;
+		this.mapSizeY = mapSizeY;
+	}
+
+	public DistanceMatrix(int mapSizeX, int mapSizeY, int[] depots, int[] clients, List<Node> bloqueados) {
+		this.mapSizeX = mapSizeX;
+		this.mapSizeY = mapSizeY;
+		this.map = new int[mapSizeX][mapSizeY];
+		this.iniMap();
+		this.matrixSize = this.mapSizeX * this.mapSizeY;
+		this.depots = depots;
+		this.clients = clients;
+		this.bloqueados = bloqueados;
+		this.createMatrix();
+	}
+
+	private void createMatrix() {
+		if (this.bloqueados != null) {
+			Bloqueo bloqueo = new Bloqueo(this.bloqueados);
+			this.matrix = bloqueo.cargarBloqueos(this.nodos, this.map, this.matrixSize, K);
+		} else {
+			this.createMatrixSinBloqueos();
+		}
+	}
+
+	private void createMatrixSinBloqueos() {
+		long[][] matrix = new long[matrixSize][matrixSize];
+
+		for (int i = 0; i < matrixSize; i++) {
+			for (int j = 0; j < matrixSize; j++) {
+				if (i == j) {
+					matrix[i][j] = 0;
+					continue;
+				}
+				// calcular distancia, si esta es igual a 1, significa que son consecutivos
+				double distance = Math.sqrt(Math.pow(this.nodos.get(i).coordY - this.nodos.get(j).coordY, 2)
+						+ Math.pow(this.nodos.get(i).coordX - this.nodos.get(j).coordX, 2));
+				if (distance==(int)distance) {
+					matrix[i][j] = (long) (distance*K);
+				} else {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+
+	}
+
+	private void iniMap() {
+		int nNode = 0;
+		nodos = new ArrayList<Node>();
+		for (int i = 0; i < this.mapSizeX; i++)
+			for (int j = 0; j < this.mapSizeY; j++) {
+				this.map[i][j] = nNode;
+				Node node = new Node(i, j);
+				nodos.add(node);
+				nNode++;
+			}
+	}
+
+	public void reset() {
+		for (int i = 0; i < this.mapSizeX; i++)
+			for (int j = 0; j < this.mapSizeY; j++)
+				this.map[i][j] = 0;
+	}
+
+	public void print() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < matrixSize; i++) {
+			for (int j = 0; j < matrixSize; j++) {
+				System.out.print(this.matrix[i][j]+" ,");
+			}
+			System.out.println();
+		}
+	}
+
+}
