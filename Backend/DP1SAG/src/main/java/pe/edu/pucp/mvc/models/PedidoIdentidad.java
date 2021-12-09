e1d7966060c258ac3bdb636831dcd5a7f2f9d729
Mario <mario.andonaire@pucp.edu.pe>
2021-12-09 02:10:41 -0500

    Insertar pedido

--- Backend/DP1SAG/src/main/java/pe/edu/pucp/mvc/models/PedidoIdentidad.java
+++ Backend/DP1SAG/src/main/java/pe/edu/pucp/mvc/models/PedidoIdentidad.java
@@ -1,10 +1,15 @@
 package pe.edu.pucp.mvc.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.IdClass;
 import java.io.Serializable;
 
 public class PedidoIdentidad implements Serializable {
 
-    private int idNodo;
     private int idExtendido;
+    private int idNodo;
 
 }
