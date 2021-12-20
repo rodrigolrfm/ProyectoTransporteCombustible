77227be53892ead361f5eb755667e4499f33eb69
Mario <mario.andonaire@pucp.edu.pe>
2021-12-20 05:08:36 -0500

    deploy

--- Backend/DP1SAG/src/main/java/pe/edu/pucp/mvc/planificacion/ScheduledTasks.java
+++ Backend/DP1SAG/src/main/java/pe/edu/pucp/mvc/planificacion/ScheduledTasks.java
@@ -85,7 +85,7 @@ public class ScheduledTasks {
         });
     }
 
-    @Scheduled(fixedRate = 120000)
+    @Scheduled(fixedRate = 300000)
     public void reportCurrentTime() throws Exception {
 
         EntidadRutas rutasFinal = EntidadRutas.builder().paths(new ArrayList<>()).build();
